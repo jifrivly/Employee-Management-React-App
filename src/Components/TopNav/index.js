e87e21fce77b24823d73ea4ded1e3b604e1f0eb6
Jifri <jifrivly@gmail.com>
2022-06-03 16:45:22 +0530

    Top nav component implemented with all routes and active css class

--- src/Components/TopNav/index.js
+++ src/Components/TopNav/index.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { ROUTES } from '../../Routes.constants';
+import { NavLink, withRouter } from 'react-router-dom';
 import PropType from 'prop-types';
-import style from './style.module.scss';
 
-function TopNav(props) {
-  const handleLogout = () => {
-    localStorage.clear();
-    props.history.push(ROUTES.LOGIN);
-  };
-  return (
-    <div className={style['nav-bar']}>
-      <div className={style.navs}>
-        <Link to={ROUTES.HOME}>
-          Home
-        </Link>
-        <Link to={ROUTES.DETAILS}>
-          Details
-        </Link>
-      </div>
-      <div className={style['user-controls']}>
-        <Link onClick={handleLogout}>
-          Logout
-        </Link>
-      </div>
-    </div>
-  );
+import { ROUTES } from '../../Routes.constants';
+// import style from './style.module.scss';
+
+class TopNav extends Component {
+    links = [
+        { name: 'Dashboard', path: ROUTES.DASHBOARD },
+        { name: 'Employees', path: ROUTES.EMPLOYEES },
+        { name: 'Clients', path: ROUTES.CLIENTS },
+        { name: 'Projects', path: ROUTES.PROJECTS },
+        { name: 'Tasks', path: ROUTES.TASKS },
+        { name: 'Calls', path: ROUTES.CALLS },
+        { name: 'Contacts', path: ROUTES.CONTACTS },
+        { name: 'Settings', path: ROUTES.SETTINGS },
+    ];
+
+    render() {
+        return (
+            <div className='navigation-block'>
+                <ul>
+                    {this.links.map((link, index) => (
+                        <li key={index}>
+                            <NavLink to={link.path} activeClassName='active'>
+                                <span>{link.name}</span>
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default withRouter(TopNav);
 
 TopNav.PropType = {
-  className: PropType.string,
+    className: PropType.string,
 };
